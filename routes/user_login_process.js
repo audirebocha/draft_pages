f74b2166114893e55e6595dd0c5168f27e8dd2eb
unknown <audire.bocha@gmail.com>
2023-06-29 10:42:17 +0000

    milestone 1 edits

---
+++ routes/user_login_process.js
@@ -0,0 +1,70 @@
+const express=require('express')
+const session=require('express-session')
+const router=express.Router()
+router.use(express.json())
+const bodyParser = require('body-parser')
+const mongoose=require('mongoose')
+const User=require('../models/user')
+const sg_mail= require('@sendgrid/mail')
+
+
+
+const API_key="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";
+sg_mail.setApiKey(API_key)
+
+
+router.use(express.static('../assets'))
+router.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+
+async function db_connect(){
+    uri="mongodb+srv://root:123@cluster0.u5atqck.mongodb.net/sharespace?retryWrites=true&w=majority"
+    try{
+        await mongoose.connect(uri);
+        console.log('Connected to Mongo DB at login process');
+    }catch(error){
+        console.error(error)
+    }
+}
+db_connect()
+
+
+//*********************************************************************************************************** */
+router.post('/',urlencodedParser,async (req,res)=>{
+    var email=req.body['username']
+    //Check if the user's email is in the database
+    result = await User.find({email:email})
+    if(result.length){
+        if(req.body['password'] === result[0]['password']){
+            if(result[0]['verified']){
+                req.session.auth=true
+                res.redirect('/')
+            }else{
+                res.redirect('/welcome')
+            }
+        }else{
+            res.json({status:"failed",message:'Wrong password',results:result})
+        }
+    }else{
+        res.json({data:req.body,params:req.params})
+    }
+})
+
+router.get('/logout',(req,res)=>{
+    req.session.destroy()
+    res.redirect('/')
+})
+
+
+
+module.exports=router
---
+++ server.js
@@ -0,0 +1,68 @@
+const express=require('express')
+const session=require('express-session')
+const bodyParser = require('body-parser')
+const user_registration_route=require('./routes/user_registration')
+const user_login_processor=require('./routes/user_login_process')
+const password_reset=require('./routes/password_reset')
+
+
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+const app= express()
+app.set("view engine",'ejs')
+app.use(express.static('./assets'))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+
+
+app.get('/',(req,res)=>{
+    random_string=Math.random().toString(36).toUpperCase().split('.')[1];
+    res.render('index',{auth:req.session.auth});
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.get('/registration',(req,res)=>{
+    res.render('registration')
+})
+
+app.get('/welcome',(req,res)=>{
+    res.render('account_activation')
+})
+
+app.get('/reset_password',(req,res)=>{
+    res.render('reset_password1')
+})
+app.get('/reset_password_reset_code',(req,res)=>{
+    res.render('reset_password2',{ email : req.session.email })
+})
+app.get('/reset_password_reset_form',(req,res)=>{
+    res.render('reset_password3',{ email : req.session.email })
+})
+app.get('/reset_password_reset_success',(req,res)=>{
+    res.render('reset_password4',{ email : req.session.email })
+})
+
+
+
+
+
+
+//************************************Route Handlers and APIs*********************************************** */
+app.use("/new_registration",user_registration_route)
+app.use('/login_processor',user_login_processor);
+app.use('/reset_password_process',password_reset);
+
+app.listen(5000,()=>{console.log("Server has started on port 5000")})
+
